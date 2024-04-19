371c605922c12f77091734aa7cc296509c84e8f8
kleabtastic <166765756+kleabtastic@users.noreply.github.com>
2024-04-19 13:07:36 -0600

    added array for pokedex data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
 
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    { number: 1, name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+    { number: 2, name: 'Ivysaur', height: 1, type: ['grass', 'poison'] },
+    { number: 3, name: 'Venusaur', height: 2, type: ['grass', 'poison'] },
+    { number: 4, name: 'Charmander', height: 0.6, type: 'fire' },
+    { number: 5, name: 'Charmeleon', height: 1.1, type: 'fire' },
+    { number: 6, name: 'Charizard', height: 0.6, type: ['fire', 'flying'] }
+]
