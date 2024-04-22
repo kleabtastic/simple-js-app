a54bd1e94b95e4c90d15da13a42cfd4089818fd4
kleabtastic <166765756+kleabtastic@users.noreply.github.com>
2024-04-22 11:17:29 -0600

    updated object key for accuracy

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 
 let pokemonList = [
-    { number: 1, name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
-    { number: 2, name: 'Ivysaur', height: 1, type: ['grass', 'poison'] },
-    { number: 3, name: 'Venusaur', height: 2, type: ['grass', 'poison'] },
-    { number: 4, name: 'Charmander', height: 0.6, type: 'fire' },
-    { number: 5, name: 'Charmeleon', height: 1.1, type: 'fire' },
-    { number: 6, name: 'Charizard', height: 0.6, type: ['fire', 'flying'] }
+    { number: 1, name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    { number: 2, name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+    { number: 3, name: 'Venusaur', height: 2, types: ['grass', 'poison'] },
+    { number: 4, name: 'Charmander', height: 0.6, types: ['fire'] },
+    { number: 5, name: 'Charmeleon', height: 1.1, types: ['fire'] },
+    { number: 6, name: 'Charizard', height: 0.6, types: ['fire', 'flying'] }
 ]
