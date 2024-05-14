5740f18c655f1455a1437d9951aba24fd4f6d76e
kleabtastic <166765756+kleabtastic@users.noreply.github.com>
2024-05-13 19:21:11 -0600

    fixed modal elements

--- js/scripts.js
+++ js/scripts.js
@@ -48,9 +48,6 @@ let pokemonRepository = (function () {
           });
         };
 
-        document.querySelector('#pokemon-class').addEventListener('click', () => {
-            showDetails()
-        });
 
     function loadDetails(item) {
         let url = item.detailsUrl;
@@ -83,35 +80,62 @@ pokemonRepository.loadList().then(function () {
     });
 });
 
+function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+};
 
-function showModal() {
+window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+
+pokemonRepository.loadDetails().then(function showModal(title, text) {
     let modalContainer = document.querySelector('#modal-container');
     modalContainer.innerHTML = '';
 
-
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+  
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+  
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+  
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+  
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
 
     modalContainer.classList.add('is-visible');
 
-    modalContainer.addEventListener('click', (e) => {
+    document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal('Modal title', 'This is the modal content!');
+      });
+
+      modalContainer.addEventListener('click', (e) => {
         let target = e.target;
         if (target === modalContainer) {
           hideModal();
         }
       });
-}
+});
+
+let closeButtonElement = document.createElement('button');
+closeButtonElement.classList.add('modal-close');
+closeButtonElement.innerText = 'Close';
+closeButtonElement.addEventListener('click', hideModal);
 
 document.querySelector('#show-modal').addEventListener('click', () => {
     showModal();
 });
 
-function hideModal() {
-    let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.remove('is-visible');
-}
 
-window.addEventListener('keydown', (e) => {
-    let modalContainer = document.querySelector('#modal-container');
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-        hideModal();
-    }
-});
+
